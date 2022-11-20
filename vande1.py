282b9b9053c6135e5dd7c51edb02567387860e50
Thang Loi <thangloitran406@gmail.com>
2022-11-20 21:04:57 +0700

    Update vande1

--- vande1.py
+++ vande1.py
@@ -1,13 +1,13 @@
 import numpy;
-import matplotlib.pyplot as plt
+from sympy import symbols, solve, Eq
+import math
 
 x = numpy.array([0, 500, 1000, 1500, 2200, 2900, 3600, 4300, 5200, 6500, 7000, 7500])
 y = numpy.array([2.93 ,2.66, 2.52, 2.4, 2.25, 2.1, 1.95, 1.8, 1.58, 1.32, 0.93, 0.39])
-p = numpy.polyfit(x, y, 1)
-print(p[0])
-print(p[1])
 
-plt.plot(x, y, 'ro-')
-plt.xlabel = ('Thời gian (s)')
-plt.ylabel = ('Lưu lượng (l/s)')
-plt.show()
+volume = 0
+for i in range(len(x)-1):
+    volume += 0.5 * (x[i+1] - x[i]) * (y[i+1] + y[i])
+
+
+print("Thể tích chất lỏng thoát ra: " + str(volume))
